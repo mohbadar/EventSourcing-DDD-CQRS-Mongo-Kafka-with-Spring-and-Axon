9d57f1da9b0138e50dfc202de9a3a4a33b71125c
mohbadar <mohammadbadarhashimi@gmail.com>
2019-11-11 15:23:35 +0430

    Setup Code base

---
+++ src/main/java/af/asr/springaxon/aggregate/DepositMoneyCommand.java
@@ -0,0 +1,18 @@
+package af.asr.springaxon.aggregate;
+
+import af.asr.springaxon.command.BaseCommand;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+public class DepositMoneyCommand extends BaseCommand<String> {
+    private BigDecimal amount;
+
+    public DepositMoneyCommand(String id, BigDecimal amount) {
+        super(id);
+        this.amount = amount;
+    }
+}
