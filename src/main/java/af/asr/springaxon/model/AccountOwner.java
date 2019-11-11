46a123b7502c35adb47ef80f7b125596a1b63ac0
mohbadar <mohammadbadarhashimi@gmail.com>
2019-11-11 15:17:48 +0430

    Setup Code base

---
+++ src/main/java/af/asr/springaxon/model/AccountOwner.java
@@ -0,0 +1,28 @@
+package af.asr.springaxon.model;
+
+
+import java.io.Serializable;
+
+public class AccountOwner implements Serializable{
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = -5357787298063565273L;
+
+    public AccountOwner() {
+
+    }
+
+    private String name;
+
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
