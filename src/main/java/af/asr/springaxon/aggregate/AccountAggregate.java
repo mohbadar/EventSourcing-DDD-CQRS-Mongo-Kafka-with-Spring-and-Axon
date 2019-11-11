1e53abb2f909597090cb1a5627194b31cc04aefd
mohbadar <mohammadbadarhashimi@gmail.com>
2019-11-11 15:20:00 +0430

    Setup Code base

--- src/main/java/af/asr/springaxon/aggregate/AccountAggregate.java
+++ src/main/java/af/asr/springaxon/aggregate/AccountAggregate.java
@@ -1,7 +1,31 @@
 package af.asr.springaxon.aggregate;
 
+import lombok.Data;
+import lombok.ToString;
+import org.axonframework.commandhandling.model.AggregateIdentifier;
+import org.axonframework.eventsourcing.EventSourcingRepository;
 import org.axonframework.spring.stereotype.Aggregate;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.math.BigDecimal;
 
 @Aggregate
+@Data
+@ToString
 public class AccountAggregate {
+
+    @Autowired
+    private EventSourcingRepository<AccountAggregate> repo;
+
+
+    @AggregateIdentifier
+    private String id;
+
+    private BigDecimal balance;
+
+    private String accountHolder;
+
+    private String accHolderName;
+
+
 }
