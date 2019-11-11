67cc79c9b1b8664b735384f0f862ead0712a27a9
mohbadar <mohammadbadarhashimi@gmail.com>
2019-11-11 16:57:13 +0430

    Command, Event and Query Implementation

--- src/main/java/af/asr/springaxon/model/Amount.java
+++ src/main/java/af/asr/springaxon/model/Amount.java
@@ -1,29 +1,19 @@
 package af.asr.springaxon.model;
 
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 import java.math.BigDecimal;
 
+@Data
+@NoArgsConstructor
 public class Amount implements Serializable{
 
-    /**
-     *
-     */
     private static final long serialVersionUID = -5684258230592753293L;
-
-    public Amount() {
-
-    }
-
     private BigDecimal amount;
 
-    public BigDecimal getAmount() {
-        return amount;
-    }
-
-    public void setAmount(BigDecimal amount) {
-        this.amount = amount;
-    }
 
 
 }
