cf784a8c0e67a69bd0d9bb3750ef27e7a99a9c2c
mohbadar <mohammadbadarhashimi@gmail.com>
2019-11-11 15:17:15 +0430

    Setup Code base

---
+++ src/main/java/af/asr/springaxon/model/Amount.java
@@ -0,0 +1,29 @@
+package af.asr.springaxon.model;
+
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+public class Amount implements Serializable{
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = -5684258230592753293L;
+
+    public Amount() {
+
+    }
+
+    private BigDecimal amount;
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+
+}
