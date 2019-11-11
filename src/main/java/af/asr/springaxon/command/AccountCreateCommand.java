92cb86b78ee14b87101fbfe1f7f14e4d59d72b19
mohbadar <mohammadbadarhashimi@gmail.com>
2019-11-11 15:21:50 +0430

    Setup Code base

--- src/main/java/af/asr/springaxon/command/AccountCreateCommand.java
+++ src/main/java/af/asr/springaxon/command/AccountCreateCommand.java
@@ -1,4 +1,29 @@
 package af.asr.springaxon.command;
 
-public class AccountCreateCommand {
-}
+public class AccountCreateCommand extends BaseCommand<String>{
+
+    private String accountHolder;
+    private String accountHolderName;
+
+    public AccountCreateCommand(String id,String holder,String accountHolderName) {
+        super(id);
+        this.accountHolder = holder;
+        this.accountHolderName = accountHolderName;
+    }
+    public AccountCreateCommand() {}
+
+    public String getAccountHolder() {
+        return this.accountHolder;
+    }
+
+    public String getAccountHolderName() {
+        return accountHolderName;
+    }
+
+    public void setAccountHolderName(String accountHolderName) {
+        this.accountHolderName = accountHolderName;
+    }
+
+
+
+
---
+++ src/main/java/af/asr/springaxon/command/BaseCommand.java
@@ -0,0 +1,21 @@
+package af.asr.springaxon.command;
+
+
+import org.axonframework.commandhandling.TargetAggregateIdentifier;
+import org.springframework.util.Assert;
+
+public class BaseCommand<T> {
+
+    @TargetAggregateIdentifier
+    private  T id;
+
+    public BaseCommand(T id) {
+        Assert.notNull(id, "Id must be not null");
+        this.id = id;
+    }
+    public BaseCommand() {}
+
+    public T getId() {
+        return id;
+    }
+}
