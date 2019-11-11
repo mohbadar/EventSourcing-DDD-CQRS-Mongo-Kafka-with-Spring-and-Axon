7a9dc27570c0acbef838ee9eee6cee1e4dbbe3ba
mohbadar <mohammadbadarhashimi@gmail.com>
2019-11-11 15:22:32 +0430

    Setup Code base

--- src/main/java/af/asr/springaxon/command/AccountCreateCommand.java
+++ src/main/java/af/asr/springaxon/command/AccountCreateCommand.java
@@ -1,29 +1,22 @@
 package af.asr.springaxon.command;
 
-public class AccountCreateCommand extends BaseCommand<String>{
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class AccountCreateCommand extends BaseCommand<String> {
 
     private String accountHolder;
     private String accountHolderName;
 
-    public AccountCreateCommand(String id,String holder,String accountHolderName) {
+    public AccountCreateCommand(String id, String holder, String accountHolderName) {
         super(id);
         this.accountHolder = holder;
         this.accountHolderName = accountHolderName;
     }
-    public AccountCreateCommand() {}
 
-    public String getAccountHolder() {
-        return this.accountHolder;
-    }
-
-    public String getAccountHolderName() {
-        return accountHolderName;
-    }
-
-    public void setAccountHolderName(String accountHolderName) {
-        this.accountHolderName = accountHolderName;
-    }
-
+}
 
 
 
